91adbe1d89dda1bbbb69d2c48dd8ad2baab25327
Sonia180 <Sonia180@users.noreply.github.com>
2024-02-12 02:00:50 -0600

    Semana04
    Semana04

---
+++ src/main/java/com/tienda_l/ProjectConfig.java
@@ -0,0 +1,40 @@
+package com.tienda_l;
+
+import java.util.Locale;
+import org.springframework.boot.autoconfigure.web.WebProperties.LocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+    
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+            var slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.getDefault());
+    slr.setLocaleAttributeName("session.current.locale");
+    slr.setTimeZoneAttributeName("session.current.timezone");
+    
+            return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+      registro.addInterceptor(localeChangeInterceptor());
+        
+    
+    }
+    
+}
